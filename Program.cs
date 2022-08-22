d111834399d6428e5928ffd3e487d05bdbb90fb3
VitaxaOv <vit2013aly@mail.ru>
2022-08-22 20:17:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Random random = new Random();
+ // Для генерации рандомных чисел
+int randomNumber = random.Next(10,100); 
+
+Console.WriteLine(randomNumber);
+
+int lastNumber = randomNumber % 10;
+int firstNumber = randomNumber / 10;
+
+if(lastNumber > firstNumber)
+{
+    Console.WriteLine($"Максимальное число равно {lastNumber}");// интерполяция
+}
+else{
+    Console.WriteLine("Максимальное число равно " + firstNumber);
+}
